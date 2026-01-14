96d91a90ddc255c719de62f4d295f2493a2fc413
MUBASHIR RAO <151456910+mubashirrao1122@users.noreply.github.com>
2026-01-14 14:55:59 +0500

    Modify agent based on location

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import 'dotenv/config'
 import {Agent, run} from "@openai/agents"
+const location = 'pakistan'
 const helloAgent = new Agent({
     name: 'hello agent',
-    instructions: "you are an agent that always says hello-world",
+    instructions: function (){
+        if (location === 'pakistan'){
+            return "Hello from Pakistan!";
+        }else if (location === 'india'){
+            return "Hello from India!";
+        }else{
+            return "Hello from the rest of the world!";
+        }
+    }
 });
 
 run(helloAgent, "hey my name is mubashir rao").then((result)=>{
